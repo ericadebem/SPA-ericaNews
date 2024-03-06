ab1756c87d4615678ff89e654aabc3e5e78ef882
Erica de Bem <ericadebemm@gmail.com>
2024-03-06 22:33:55 +0100

    Add axios and react-router-dom dependencies, update page title

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,46 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Navbar } from "./components/Navbar/Navbar";
+import Home from "./pages/Home/Home";
+import { Search } from "./pages/Search/Search";
+import { GlobalStyled } from "./GlobalStyled";
+import ErrorPage from "./pages/ErrorPage/ErrorPage";
+import { Authentication } from "./pages/Authentication/Authentication";
+import { Profile } from "./pages/Profile/Profile";
+import UserProvider from "./Context/UserContext";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Navbar />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/search/:title",
+        element: <Search />,
+      },
+      {
+        path: "/profile",
+        element: <Profile />,
+      },
+    ],
+  },
+  {
+    path: "/auth",
+    element: <Authentication />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <GlobalStyled />
+    <UserProvider>
+      <RouterProvider router={router} />
+    </UserProvider>
+  </React.StrictMode>
+);
